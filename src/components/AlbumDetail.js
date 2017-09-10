de40a268c74ce8ab807baa7fafd8d2c669a4f002
Katherin Demyanenko <katherindemyanenko@MacBook-Air-Katherin.local>
2017-09-10 12:08:41 +0300

    Wrap up commit

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -29,7 +29,9 @@ const AlbumDetail = ({ album }) => {
         <Image style={albumImage} source={{ uri: image }} />
       </CardSection>
       <CardSection>
-        <Button onPress={() => Linking.openURL(url)}/>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy now!
+        </Button>
       </CardSection>
     </Card>
   )
